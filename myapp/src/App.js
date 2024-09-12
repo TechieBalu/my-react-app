516386e5de6078ffadd2ae6504afcb933e749594
TechieBalu <balu040816@gmail.com>
2024-09-12 16:07:07 +0530

    Updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing
+          Testing of my choice.
         </p>
         <a
           className="App-link"
